bab1b1d37cb5e4e8fa8a780710f51b6b112924c4
Spencer Herzberg <spencer.herzberg@gmail.com>
2014-05-31 15:24:46 +0000

    install fig version 0.4.1 on template

--- template/puppet/modules/roles/manifests/template.pp
+++ template/puppet/modules/roles/manifests/template.pp
@@ -12,4 +12,15 @@ class roles::template {
     image_tag => '14.04',
   }
 
+  include wget
+
+  wget::fetch {'https://github.com/orchardup/fig/releases/download/0.4.1/linux':
+    destination => '/usr/local/bin/fig',
+  }
+  ->
+  file {'/usr/local/bin/fig':
+    ensure => present,
+    mode   => 555,
+  }
+
 }
